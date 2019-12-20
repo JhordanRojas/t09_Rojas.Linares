5107332da6ca0d31f7dfeb86419f98d74601bbc7
JhordanRojas <56855169+jhordanrojas@users.noreply.github.com>
2019-12-20 13:31:43 -0500

    se agrego el vigesimo ejercicio

--- Rojas/app_20.py
+++ Rojas/app_20.py
@@ -1,10 +1,10 @@
 #20. Funcion que calcula el promedio de tres numeros
 import os
-from Rojas.librerio import *
+import library
 
 n1=os.sys.argv[1]
 n2=os.sys.argv[2]
 n3=os.sys.argv[3]
 
-prome=promedio(n1,n2,n3)
+prome=library.promedio(n1,n2,n3)
 print("el valor del promedio es:",prome)
