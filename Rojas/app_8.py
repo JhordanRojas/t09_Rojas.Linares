66711906ead3b92e409940680db5cb87ca3b1d97
JhordanRojas <56855169+jhordanrojas@users.noreply.github.com>
2019-12-20 13:36:47 -0500

    se agrego el octavo ejercicio

--- Rojas/app_8.py
+++ Rojas/app_8.py
@@ -1,10 +1,10 @@
 #8. Funcion que valida si un numero es mimport os
 import os
-from Rojas.librerio import *
+import library
 
 numero=os.sys.argv[1]
 
-eight=multiplo_de_5(numero)
+eight=library.multiplo_de_5(numero)
 if eight==True:
     print(numero," si es miltiplo de 5")
 else:
