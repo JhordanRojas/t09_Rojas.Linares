533f57a76dd3665d122ff8a089f48d6324a307fa
JhordanRojas <56855169+jhordanrojas@users.noreply.github.com>
2019-12-20 13:33:20 -0500

    se agrego el vigesimo cuarto ejercicio

--- Rojas/app_24.py
+++ Rojas/app_24.py
@@ -1,9 +1,9 @@
 #24. Funcion que la fuerza de un cuerpo
 import os
-from Rojas.librerio import *
+import library
 
 n1=os.sys.argv[1]
 n2=os.sys.argv[2]
 
-cacl=fuerza(n1,n2)
+cacl=library.fuerza(n1,n2)
 print("el valor de la fuerza es:",cacl)
