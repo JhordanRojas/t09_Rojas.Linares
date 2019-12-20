3452eba1c7795930842fad7038bb2ed6f97c7bf2
JhordanRojas <56855169+jhordanrojas@users.noreply.github.com>
2019-12-20 13:29:02 -0500

    se agrego el catorceavo ejercicio

--- Rojas/app_14.py
+++ Rojas/app_14.py
@@ -1,8 +1,8 @@
 #14. Funcion que calcula la sumatoria de numeros naturales
 import os
-from Rojas.librerio import *
+import library
 
 sumandos=os.sys.argv[1]
 
-sumt=sumatoria_nat(sumandos)
+sumt=library.sumatoria_nat(sumandos)
 print("la sumtoria de los ",sumandos," numeros naturales es:",sumt)
