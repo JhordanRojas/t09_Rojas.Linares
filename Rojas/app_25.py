c70a138b5f3cff457e4c32bdc809d5c9e4653975
JhordanRojas <56855169+jhordanrojas@users.noreply.github.com>
2019-12-20 13:33:45 -0500

    se agrego el vigesimo quinto ejercicio

--- Rojas/app_25.py
+++ Rojas/app_25.py
@@ -1,9 +1,8 @@
 #25. Funcion que calcula la suma de los n primeros cuadrados perfectos
 import os
-from Rojas.library import *
+import library
 
 n_elembntos=os.sys.argv[1]
 
-
-sumatr=sum_cuadrados(n_elembntos)
+sumatr=library.sum_cuadrados(n_elembntos)
 print("el valor de la sumatoria de los",n_elembntos,"primeros cuadrados es:",sumatr)
