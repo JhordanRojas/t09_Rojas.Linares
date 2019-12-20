d790b7f3a0c384b1d79ad6191dedec3eb2798c60
JhordanRojas <56855169+jhordanrojas@users.noreply.github.com>
2019-12-20 13:31:16 -0500

    se agrego el decimo noveno ejercicio

--- Rojas/app_19.py
+++ Rojas/app_19.py
@@ -1,10 +1,10 @@
 #19. Funcion multiplicar 3 numeros
 import os
-from Rojas.librerio import *
+import library
 
 n1=os.sys.argv[1]
 n2=os.sys.argv[2]
 n3=os.sys.argv[3]
 
-multi=mult_3(n1,n2,n3)
+multi=library.mult_3(n1,n2,n3)
 print("el valor de la multiplicacion es:",multi)
