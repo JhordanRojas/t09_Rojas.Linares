a8bce7ff94f319d0005aa5b1a7534d62fdc93a29
JhordanRojas <56855169+jhordanrojas@users.noreply.github.com>
2019-12-20 13:28:20 -0500

    se agrego el doceavo ejercicio

--- Rojas/app_12.py
+++ Rojas/app_12.py
@@ -1,9 +1,8 @@
 #12. Funcion que calcula la densidad de un cuerpo
 import os
-from Rojas.librerio import *
-
+import library
 mas=os.sys.argv [1]
 vol=os.sys.argv[2]
 
-twlve=densidad(mas,vol)
+twlve=library.densidad(mas,vol)
 print("la densidad del cuerpo es:",twlve)
